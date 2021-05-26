7b5375fc014d6d3d5dd50906ecabfcd04ac440c3
Omar <omaar_5@hotmail.com>
2021-05-27 00:11:06 +0200

    feat: Header part-2

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,9 +1,9 @@
 import Image from "next/image";
 import {
-    BellIcon,
-    ChatIcon,
     HomeIcon,
     UserGroupIcon,
+    BellIcon,
+    ChatIcon,
     ViewGridIcon,
 } from "@heroicons/react/solid";
 import {
@@ -12,10 +12,11 @@ import {
     PlayIcon,
     ShoppingCartIcon,
 } from "@heroicons/react/outline";
+import { HeaderIcon } from "..";
 
 const Header = () => {
     return (
-        <div>
+        <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
             <div className="flex items-center">
                 <div>
                     <Image
@@ -29,16 +30,21 @@ const Header = () => {
                 <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
                     <SearchIcon className="h-6 text-gray-500" />
                     <input
-                        className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500"
+                        className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500
+                        flex-shrink"
                         type="text"
                         placeholder="Search Facebook"
                     />
                 </div>
             </div>
 
-            <div>
-                <div>
-                    <HeaderIcon Icon={HomeIcon} />
+            <div className="flex justify-center flex-grow">
+                <div className="flex space-x-6 md:space-x-2">
+                    <HeaderIcon Icon={HomeIcon} active />
+                    <HeaderIcon Icon={FlagIcon} />
+                    <HeaderIcon Icon={PlayIcon} />
+                    <HeaderIcon Icon={ShoppingCartIcon} />
+                    <HeaderIcon Icon={UserGroupIcon} />
                 </div>
             </div>
         </div>
