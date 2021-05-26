5578e54346b7f1ae971559f97d324b183308c497
Omar <omaar_5@hotmail.com>
2021-05-26 22:20:54 +0200

    test: tailwindcss

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,10 @@ import HeadTag from '../global/HeadTag';
 
 const Home = () => {
   return (
-    <div>
+    <div className="flex">
       <HeadTag title="Facebook 2.0 Clone" />
 
+      <h1>Let's Build Facebook Clone</h1>
       <h1>Let's Build Facebook Clone</h1>
     </div>
   );
