51624091c4c853427809d91ca4bc1c3388ac20c7
Omar <omaar_5@hotmail.com>
2021-05-26 22:26:35 +0200

    feat: setup Heade Component

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
+import { Header } from '../components';
 import HeadTag from '../global/HeadTag';
 
 const Home = () => {
   return (
-    <div className="flex">
+    <div>
       <HeadTag title="Facebook 2.0 Clone" />
 
-      <h1>Let's Build Facebook Clone</h1>
-      <h1>Let's Build Facebook Clone</h1>
+      <Header />
     </div>
   );
 };
