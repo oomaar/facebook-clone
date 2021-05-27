f81391f04e266a1b4ecc3b58e736cb343a923b35
Omar <omaar_5@hotmail.com>
2021-05-27 15:48:00 +0200

    feat: Header part-3

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -2,21 +2,22 @@ import Image from "next/image";
 import {
     HomeIcon,
     UserGroupIcon,
-    BellIcon,
-    ChatIcon,
     ViewGridIcon,
+    ChatIcon,
+    BellIcon,
 } from "@heroicons/react/solid";
 import {
     SearchIcon,
     FlagIcon,
     PlayIcon,
     ShoppingCartIcon,
+    ChevronDownIcon,
 } from "@heroicons/react/outline";
 import { HeaderIcon } from "..";
 
 const Header = () => {
     return (
-        <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
+        <div className=" sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
             <div className="flex items-center">
                 <div>
                     <Image
@@ -30,8 +31,8 @@ const Header = () => {
                 <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
                     <SearchIcon className="h-6 text-gray-500" />
                     <input
-                        className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500
-                        flex-shrink"
+                        className="hidden md:inline-flex ml-2 items-center bg-transparent
+                        outline-none placeholder-gray-500 flex-shrink"
                         type="text"
                         placeholder="Search Facebook"
                     />
@@ -46,6 +47,15 @@ const Header = () => {
                     <HeaderIcon Icon={ShoppingCartIcon} />
                     <HeaderIcon Icon={UserGroupIcon} />
                 </div>
+            </div>
+
+            <div className="flex items-center sm:space-x-2 justify-end">
+                {/* User Picture */}
+                <p className="font-semibold pr-3 whitespace-nowrap">Username</p>
+                <ViewGridIcon className="icon" />
+                <ChatIcon className="icon" />
+                <BellIcon className="icon" />
+                <ChevronDownIcon className="icon" />
             </div>
         </div>
     );
