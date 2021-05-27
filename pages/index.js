49fa66d0c3a344c0b8567b1dffd306f8da857f6c
Omar <omaar_5@hotmail.com>
2021-05-27 19:52:36 +0200

    feat: setup sidebar

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,16 @@
-import { Header, Login } from '../components';
+import { Header, Login, Sidebar } from '../components';
 import HeadTag from '../global/HeadTag';
 import { getSession } from "next-auth/client";
 
 const Home = ({ session }) => {
   if (!session) return <Login />;
+
   return (
     <div>
       <HeadTag title="Facebook 2.0 Clone" />
 
       <Header />
+      <Sidebar />
     </div>
   );
 };
