03895e642e30e6e3ddb92dd045d9a57c3968532d
Omar <omaar_5@hotmail.com>
2021-05-29 16:37:26 +0200

    feat: Sidebar Part-1

--- components/Sidebar/Sidebar.js
+++ components/Sidebar/Sidebar.js
@@ -1,7 +1,22 @@
+import { useSession } from "next-auth/client";
+import {
+    ChevronDownIcon,
+    ShoppingBagIcon,
+    UserGroupIcon,
+} from "@heroicons/react/solid";
+import {
+    CalendarIcon,
+    ClockIcon,
+    DesktopComputerIcon,
+    UsersIcon,
+} from "@heroicons/react/outline";
+
 const Sidebar = () => {
+    const [session, loading] = useSession();
+
     return (
-        <div>
-            Sidebar
+        <div className="p-2 mt-5 max-w-[600px] xl:min-w-[300px]">
+            {/* <SidebarRow Icon /> */}
         </div>
     );
 };
