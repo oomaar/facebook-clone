d8436194c63a9fb117df25c2f609bf5e081d3c50
Omar <omaar_5@hotmail.com>
2021-05-29 17:14:34 +0200

    feat: SidebarRow part-1

--- components/index.js
+++ components/index.js
@@ -2,3 +2,4 @@ export { default as Header } from "./Header/Header";
 export { default as HeaderIcon } from "./HeaderIcon/HeaderIcon";
 export { default as Login } from "./Login/Login";
 export { default as Sidebar } from "./Sidebar/Sidebar";
+export { default as SidebarRow } from "./SidebarRow/SidebarRow";
