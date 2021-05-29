f78f5ffe3a3f33014881e67e59da6de517c754fd
Omar <omaar_5@hotmail.com>
2021-05-29 17:14:45 +0200

    feat: Sidebar part-2

--- components/Sidebar/Sidebar.js
+++ components/Sidebar/Sidebar.js
@@ -1,14 +1,15 @@
 import { useSession } from "next-auth/client";
+import { SidebarRow } from "..";
 import {
-    ChevronDownIcon,
-    ShoppingBagIcon,
     UserGroupIcon,
+    ShoppingBagIcon,
+    ChevronDownIcon,
 } from "@heroicons/react/solid";
 import {
+    UsersIcon,
+    DesktopComputerIcon,
     CalendarIcon,
     ClockIcon,
-    DesktopComputerIcon,
-    UsersIcon,
 } from "@heroicons/react/outline";
 
 const Sidebar = () => {
@@ -16,7 +17,14 @@ const Sidebar = () => {
 
     return (
         <div className="p-2 mt-5 max-w-[600px] xl:min-w-[300px]">
-            {/* <SidebarRow Icon /> */}
+            <SidebarRow src={session.user.image} title={session.user.name} />
+            <SidebarRow Icon={UsersIcon} title="Friends" />
+            <SidebarRow Icon={UserGroupIcon} title="Groups" />
+            <SidebarRow Icon={ShoppingBagIcon} title="Marketplace" />
+            <SidebarRow Icon={DesktopComputerIcon} title="DesktopWatch" />
+            <SidebarRow Icon={CalendarIcon} title="Events" />
+            <SidebarRow Icon={ClockIcon} title="Memories" />
+            <SidebarRow Icon={ChevronDownIcon} title="See More" />
         </div>
     );
 };
