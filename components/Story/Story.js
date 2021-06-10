9c1241944af072e73d5fcdd4d1a890e685e98f35
Omar <omaar_5@hotmail.com>
2021-06-10 23:40:34 +0200

    feat: Story part-1

--- components/Story/Story.js
+++ components/Story/Story.js
@@ -1,34 +1,13 @@
 import { StoryCard } from "..";
+import { stories } from "../../data/storiesData";
 
-const stories = [
-    {
-        name: "Omar Hassan",
-        src: "/story/Me.jpg",
-        profile: "/story/Me2.JPG"
-    },
-    {
-        name: "Elon Musk",
-        src: "/story/Elon Musk.jpg",
-        profile: "/story/Elon Musk2.jpg"
-    },
-    {
-        name: "Mark Zuckerberg",
-        src: "/story/Mark Zuckerberg.jpg",
-        profile: "/story/Mark Zuckerberg2.jpg"
-    },
-    {
-        name: "Bill Gates",
-        src: "/story/Bill Gates.jpg",
-        profile: "/story/Bill Gates2.jpg"
-    }
-];
 
 const Story = () => {
     return (
         <div className="flex justify-center space-x-3 mx-auto">
-            story
-            {stories.map(story => (
+            {stories.map((story, i) => (
                 <StoryCard
+                    key={i}
                     name={story.name}
                     src={story.src}
                     profile={story.profile}
