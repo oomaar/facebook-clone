598b447e0451c94de021596a9a490fea91af67a3
Omar <omaar_5@hotmail.com>
2021-06-10 23:09:42 +0200

    feat: setup componentsData

---
+++ data/sidebarData.js
@@ -0,0 +1,49 @@
+import {
+    UserGroupIcon,
+    ShoppingBagIcon,
+    ChevronDownIcon,
+} from "@heroicons/react/solid";
+import {
+    UsersIcon,
+    DesktopComputerIcon,
+    CalendarIcon,
+    ClockIcon,
+} from "@heroicons/react/outline";
+
+export const sidebarRowData = [
+    {
+        id: 1,
+        Icon: UsersIcon,
+        title: "Freinds"
+    },
+    {
+        id: 2,
+        Icon: UserGroupIcon,
+        title: "Groups"
+    },
+    {
+        id: 3,
+        Icon: ShoppingBagIcon,
+        title: "Marketplace"
+    },
+    {
+        id: 4,
+        Icon: DesktopComputerIcon,
+        title: "DesktopWatch"
+    },
+    {
+        id: 5,
+        Icon: CalendarIcon,
+        title: "Events"
+    },
+    {
+        id: 6,
+        Icon: ClockIcon,
+        title: "Memories"
+    },
+    {
+        id: 7,
+        Icon: ChevronDownIcon,
+        title: "See More"
+    }
+];
---
+++ data/storiesData.js
@@ -0,0 +1,22 @@
+export const stories = [
+    {
+        name: "Omar Hassan",
+        src: "/story/Me.jpg",
+        profile: "/story/Me2.JPG"
+    },
+    {
+        name: "Elon Musk",
+        src: "/story/Elon Musk.jpg",
+        profile: "/story/Elon Musk2.jpg"
+    },
+    {
+        name: "Mark Zuckerberg",
+        src: "/story/Mark Zuckerberg.jpg",
+        profile: "/story/Mark Zuckerberg2.jpg"
+    },
+    {
+        name: "Bill Gates",
+        src: "/story/Bill Gates.jpg",
+        profile: "/story/Bill Gates2.jpg"
+    }
+];
