738d14955aa68a37f494a2a0b4918469664f138d
Omar <omaar_5@hotmail.com>
2021-06-11 22:02:51 +0200

    feat: Widgets part-2 (complete Contact)

--- components/Contact/Contact.js
+++ components/Contact/Contact.js
@@ -1,7 +1,18 @@
-const Contact = () => {
+import Image from "next/image";
+
+const Contact = ({ src, name }) => {
     return (
-        <div>
-            Contact
+        <div className="flex items-center space-x-3 mb-2 relative hover:bg-gray-200 cursor-pointer p-2 rounded-xl">
+            <Image
+                className="rounded-full"
+                objectFit="cover"
+                src={src}
+                width={50}
+                height={50}
+                layout="fixed"
+            />
+            <p>{name}</p>
+            <div className="absolute bottom-2 left-7 bg-green-400 h-3 w-3 rounded-full" />
         </div>
     );
 };
