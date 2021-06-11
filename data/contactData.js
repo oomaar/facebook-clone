1e1033d65821c7ac95b184d0d2327685e51fb36d
Omar <omaar_5@hotmail.com>
2021-06-11 21:46:18 +0200

    setup: contactsData

---
+++ data/contactData.js
@@ -0,0 +1,30 @@
+export const contacts = [
+    {
+        src: "/contacts/jeff.jpg",
+        name: "Jeff Bezos"
+    },
+    {
+        src: "/contacts/elon.jpg",
+        name: "Elon Musk"
+    },
+    {
+        src: "/contacts/bill.jpg",
+        name: "Bill Gates"
+    },
+    {
+        src: "/contacts/mark.jpg",
+        name: "Mark Zuckerberg"
+    },
+    {
+        src: "/contacts/harry.jpg",
+        name: "Harry Potter"
+    },
+    {
+        src: "/contacts/ukqueen.jpg",
+        name: "The Queen"
+    },
+    {
+        src: "/contacts/bond.jpg",
+        name: "James Bond"
+    },
+];
