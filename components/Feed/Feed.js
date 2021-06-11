ae32c5cf076374cd866e76e7ff95651c2439389b
Omar <omaar_5@hotmail.com>
2021-06-11 18:32:14 +0200

    style: use scrollbar-hide

--- components/Feed/Feed.js
+++ components/Feed/Feed.js
@@ -2,7 +2,7 @@ import { InputBox, Posts, Story } from "..";
 
 const Feed = () => {
     return (
-        <div className="flex-grow h-screen pb-44 pt-6 mr-4 xl:mr-40 overflow-y-auto">
+        <div className="flex-grow h-screen pb-44 pt-6 mr-4 xl:mr-40 overflow-y-auto scrollbar-hide">
             <div className="mx-auto max-w-md md:max-w-lg">
                 <Story />
                 <InputBox />
