2a0990a9b5224b3ce85729ddc072f0c0f39e3464
Omar <omaar_5@hotmail.com>
2021-06-11 21:46:33 +0200

    feat: Widgets Part-1

--- components/Widgets/Widgets.js
+++ components/Widgets/Widgets.js
@@ -1,7 +1,30 @@
+import { Contact } from "..";
+import { contacts } from "../../data/contactData";
+import { SearchIcon } from "@heroicons/react/outline";
+import {
+    DotsHorizontalIcon,
+    VideoCameraIcon
+} from "@heroicons/react/solid";
+
 const Widgets = () => {
     return (
-        <div>
-            Widgets
+        <div className="hidden md:inline-flex flex-col w-60 p-2 mt-5">
+            <div className="flex justify-between items-center text-gray-500 mb-5">
+                <h2 className="text-xl">Contacts</h2>
+                <div className="flex space-x-2">
+                    <VideoCameraIcon className="h-6" />
+                    <SearchIcon className="h-6" />
+                    <DotsHorizontalIcon className="h-6" />
+                </div>
+            </div>
+
+            {contacts.map((contact, i) => (
+                <Contact
+                    key={i}
+                    src={contact.src}
+                    name={contact.name}
+                />
+            ))}
         </div>
     );
 };
