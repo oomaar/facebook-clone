dc1e5158cb0e15e9f2d8ffd904adf6bbf56fa114
Omar <omaar_5@hotmail.com>
2021-06-11 18:07:31 +0200

    feat: posts part-1

--- components/Posts/Posts.js
+++ components/Posts/Posts.js
@@ -1,7 +1,27 @@
+import { useCollection } from "react-firebase-hooks/firestore";
+import { db } from "../../lib/Firebase";
+import { Post } from "..";
+
 const Posts = () => {
+    const [realtimePosts, loading, error] = useCollection(
+        db.
+            collection('posts').
+            orderBy('timestamp', 'desc')
+    );
+
     return (
         <div>
-            Posts
+            {realtimePosts?.docs.map(post => (
+                <Post
+                    key={post.id}
+                    name={post.data().name}
+                    message={post.data().message}
+                    email={post.data().email}
+                    timestamp={post.data().timestamp}
+                    image={post.data().image}
+                    postImage={post.data().postImage}
+                />
+            ))}
         </div>
     );
 };
