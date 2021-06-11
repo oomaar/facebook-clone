31bd83793d7927c27e1cbc178da5641693a2f188
Omar <omaar_5@hotmail.com>
2021-06-11 22:18:55 +0200

    feat: serverSideRender posts loading fast

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import { Feed, Header, Login, Sidebar, Widgets } from '../components';
 import HeadTag from '../global/HeadTag';
 import { getSession } from "next-auth/client";
+import { db } from '../lib/Firebase';
 
-const Home = ({ session }) => {
+const Home = ({ session, posts }) => {
   if (!session) return <Login />;
 
   return (
@@ -13,7 +14,7 @@ const Home = ({ session }) => {
 
       <main className="flex">
         <Sidebar />
-        <Feed />
+        <Feed posts={posts} />
         <Widgets />
       </main>
     </div>
@@ -24,10 +25,21 @@ export default Home;
 
 export async function getServerSideProps(context) {
   const session = await getSession(context);
+  const posts = await db
+    .collection('posts')
+    .orderBy('timestamp', 'desc')
+    .get();
+
+  const docs = posts.docs.map(post => ({
+    id: post.id,
+    ...post.data(),
+    timestamp: null
+  }));
 
   return {
     props: {
-      session
+      session,
+      posts: docs
     }
   };
 };
